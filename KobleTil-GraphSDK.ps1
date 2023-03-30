c38f58aa2af2cbc271fb1aeccda13fc77ca2cbc8
Tom Rune Berg <tomrune@knowledgegroup.no>
2023-03-30 17:41:23 +0200

    Lagt til bedre forklaringer

--- KobleTil-GraphSDK.ps1
+++ KobleTil-GraphSDK.ps1
@@ -1,13 +1,14 @@
-## Koble til Mg (Microsoft Graph): 
+## Koble til Mg (Microsoft Graph), verdier fra Azure App-Registration og sertifikat: 
 $azureMGAppId = ""
 $tenantID = ""
 $azureMGCertThumbprint = ""
 
+# Koble til Graph API
 $mgGraphConnection = Connect-MgGraph -ClientId $azureMGAppId -TenantId $tenantID -CertificateThumbprint $azureMGCertThumbprint
-$mgGraphConnection
+$mgGraphConnection # Returnerer en tekststreng om tilkoblet
 
 # Søk gjennom alle INSTALLERTE  moduler etter kommandoer knyttet til gitt tema, f.eks. Teams: 
 Get-Command -Module Microsoft.Graph* *team*
 
 ## Teams Tag-funskjon er nå GA, så det er ikke lenger nødvendig å bytte til Beta endpoint
-#Select-MgProfile -Name "beta" # alt "v1.0" som er standard
+Select-MgProfile -Name "beta" # alt "v1.0" som er standard
