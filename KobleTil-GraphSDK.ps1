e793f5449ab0d823900be29d6446882a3469f1dc
Tom Rune Berg <tomrune@knowledgegroup.no>
2023-03-30 17:33:16 +0200

    Oppdatert for å støtte CSV-opprettelse

--- KobleTil-GraphSDK.ps1
+++ KobleTil-GraphSDK.ps1
@@ -4,9 +4,10 @@ $tenantID = ""
 $azureMGCertThumbprint = ""
 
 $mgGraphConnection = Connect-MgGraph -ClientId $azureMGAppId -TenantId $tenantID -CertificateThumbprint $azureMGCertThumbprint
+$mgGraphConnection
 
 # Søk gjennom alle INSTALLERTE  moduler etter kommandoer knyttet til gitt tema, f.eks. Teams: 
 Get-Command -Module Microsoft.Graph* *team*
 
-# Teams Tag-funskjon er i skrivende stund tilgjengelig via Beta endpoint - dette bytter vi til med:
-Select-MgProfile -Name "beta" # alt "v1.0" som er standard
+## Teams Tag-funskjon er nå GA, så det er ikke lenger nødvendig å bytte til Beta endpoint
+#Select-MgProfile -Name "beta" # alt "v1.0" som er standard
